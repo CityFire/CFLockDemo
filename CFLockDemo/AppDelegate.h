768e0db5c9eaacc5b74ec39436292e6ae2878618
CityFire <13787697167@163.com>
2017-04-18 16:58:03 +0800

    Initial Commit

---
+++ CFLockDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CFLockDemo
+//
+//  Created by wjc on 2017/4/18.
+//  Copyright © 2017年 CityFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CFLockDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CFLockDemo
+//
+//  Created by wjc on 2017/4/18.
+//  Copyright © 2017年 CityFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
